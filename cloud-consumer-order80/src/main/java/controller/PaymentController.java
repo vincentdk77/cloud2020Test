c701db0e8714aa3f126d0e90b385622596769257
vicentdk77 <26166405+vincentdk77@users.noreply.github.com>
2020-09-21 20:25:28 +0800

    provider,consumer

---
+++ cloud-consumer-order80/src/main/java/controller/PaymentController.java
@@ -0,0 +1,12 @@
+package controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+@RestController
+@Slf4j
+public class PaymentController {
+
+}
