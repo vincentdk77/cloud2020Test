8a58f7315c6720544dc4a2fe4bd444dd7d56b7d8
vicentdk77 <26166405+vincentdk77@users.noreply.github.com>
2020-09-25 11:43:54 +0800

    添加Spring cloud GateWay服务

---
+++ cloud-gateway-gateway9527/src/main/java/com/atguigu/springcloud/config/GateWayConfig.java
@@ -0,0 +1,21 @@
+package com.atguigu.springcloud.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 两种方式配置spring cloud gateway 路由：代码配置与yml中配置
+ */
+@Configuration
+public class GateWayConfig {
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder routeLocatorBuilder) {
+        RouteLocatorBuilder.Builder routes = routeLocatorBuilder.routes();
+        //浏览器输入 http://localhost:9527/guonei，即可跳转到 http://news.baidu.com/guonei
+        routes.route("path_rote_atguigu", r -> r.path("/guonei").uri("http://news.baidu.com/guonei")).build();
+        return routes.build();
+    }
+
+}
