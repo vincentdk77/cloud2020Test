7b9d70ae5652da9c93935c3097c713dc928d01b3
vicentdk77 <26166405+vincentdk77@users.noreply.github.com>
2020-09-30 16:51:41 +0800

    添加sentinel

---
+++ cloudalibaba-sentinel-service8401/src/main/java/com/atguigu/springcloud/SentinelServiceMain8401.java
@@ -0,0 +1,15 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class SentinelServiceMain8401
+{
+    public static void main(String[] args) {
+        SpringApplication.run(SentinelServiceMain8401.class, args);
+    }
+}
